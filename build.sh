995aaff64210b285dac23d449e88767273b5f8ac
Nobin Dwi Adi Saputra <edwardmiles91@gmail.com>
2018-11-24 09:08:19 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,8 @@
+cd ~/kernel/riva-kernel/
+export CROSS_COMPILE=~/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export ARCH=arm64 && export SUBARCH=arm64
+mkdir -p out
+make O=out clean
+make O=out mrproper
+make O=out msm8937-perf_defconfig
+make O=out -j$(nproc --all)
