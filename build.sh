93e51e4a7955c97586eb30d56315e3be2215dd4f
Nobin Dwi Adi Saputra <edwardmiles91@gmail.com>
2018-11-24 09:20:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-cd ~/kernel/riva-kernel/
-export CROSS_COMPILE=~/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/user/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ARCH=arm64 && export SUBARCH=arm64
 mkdir -p out
 make O=out clean
 make O=out mrproper
 make O=out msm8937-perf_defconfig
-make O=out -j$(nproc --all)
+make O=out -j2
